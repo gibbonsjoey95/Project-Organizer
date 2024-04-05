c08487cee668849c036be7bdbe55363add53af5c
Joey <gibbonsjoey95@gmail.com>
2024-04-04 21:39:22 -0500

    set up basic styling to work with

--- src/index.js
+++ src/index.js
@@ -1,36 +1,7 @@
 import './styles.css'
 
 const component = () => {
-    const content = document.querySelector('#content')
-
-    const headDiv = document.createElement('div')
-    
-    const title = document.createElement('h1')
-    title.textContent = "My To-do's"
-
-    const bodyDiv = document.createElement('div')
-
-    const addTodoButton = document.createElement('button')
-    addTodoButton.textContent = "Create To-do"
-
-    let value = 0
-
-    addTodoButton.addEventListener('click', () => {
-        const todo = document.createElement('p')
-
-        todo.textContent = value
-
-        bodyDiv.appendChild(todo)
-        value++
-    })
-
-    headDiv.appendChild(title)
-    content.appendChild(headDiv)
-
-    bodyDiv.appendChild(addTodoButton)
-    content.appendChild(bodyDiv)
-
-    return content
+     
 }
 
 document.body.appendChild(component())
