2b3f3ac2ffb8244fadf72171767fbebffede79b5
Joey <gibbonsjoey95@gmail.com>
2024-04-10 13:42:20 -0500

    set up todo class

---
+++ src/todo.js
@@ -0,0 +1,12 @@
+class Todo {
+    constructor(title, description, dueDate, priority, checklist, notes) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.checklist = checklist;
+        this.notes = notes
+    }
+}
+
+export { Todo }
